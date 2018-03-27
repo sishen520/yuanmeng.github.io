4623c1a652f79d70b72d9a924cfefa988e88aaba
sishen520 <34979795+sishen520@users.noreply.github.com>
2018-03-28 01:17:03 +0800

    Add files via upload

---
+++ .gitignore/style/functions.js
@@ -0,0 +1,64 @@
+/*
+	最新2.1版本应该完美解决时间不准确的问题了，感谢网友的积极反馈，研究这些问题对我的学习有重要帮助，再次感谢！！！
+	另外，我的编程博客已经开放。
+	Deboy博客：www.deboy.cn
+	我的博客微信：deboyblog 欢迎订阅，我们一起讨论编程问题~
+	我的业务QQ：915099705
+	编程学习加QQ：55631825 一起学习，一起进步~
+ */
+
+// variables
+var $win = $(window);
+var clientWidth = $win.width();
+var clientHeight = $win.height();
+
+$(window).resize(function() {
+    var newWidth = $win.width();
+    var newHeight = $win.height();
+    if (newWidth != clientWidth && newHeight != clientHeight) {
+        location.replace(location);
+    }
+});
+
+(function($) {
+	$.fn.typewriter = function() {
+		this.each(function() {
+			var $ele = $(this), str = $ele.html(), progress = 0;
+			$ele.html('');
+			var timer = setInterval(function() {
+				var current = str.substr(progress, 1);
+				if (current == '<') {
+					progress = str.indexOf('>', progress) + 1;
+				} else {
+					progress++;
+				}
+				$ele.html(str.substring(0, progress) + (progress & 1 ? '_' : ''));
+				if (progress >= str.length) {
+					clearInterval(timer);
+				}
+			}, 75);
+		});
+		return this;
+	};
+})(jQuery);
+
+function timeElapse(date){
+	var current = Date();
+	var date3 = (Date.parse(current) - Date.parse(date) + 2678400000) ;
+	/*时间差的毫秒数，
+	  别问我后面那一堆长长的数字是什么，javascript时间计算机制惹的祸！！！！
+	*/
+	//计算出相差天数
+	var days=Math.floor(date3/(24*3600*1000))
+	//计算出小时数
+	var leave1=date3%(24*3600*1000)    //计算天数后剩余的毫秒数
+	var hours=Math.floor(leave1/(3600*1000))
+	//计算相差分钟数
+	var leave2=leave1%(3600*1000)        //计算小时数后剩余的毫秒数
+	var minutes=Math.floor(leave2/(60*1000))
+	//计算相差秒数
+	var leave3=leave2%(60*1000)      //计算分钟数后剩余的毫秒数
+	var seconds=Math.round(leave3/1000)
+	var result = "<span class=\"digit\">" + days + "</span> 天 <span class=\"digit\">" + hours + "</span> 时 <span class=\"digit\">" + minutes + "</span> 分 <span class=\"digit\">" + seconds + "</span> 秒"; 
+	$("#clock").html(result);
+}
